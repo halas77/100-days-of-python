1b5238bfd1c0f2cbf531ceed0105b7977d731156
Dawit Mellese <dawitm777@gmail.com>
2023-12-17 22:17:41 +0300

    First day Commit

---
+++ day-01/index.py
@@ -0,0 +1,54 @@
+
+# print function 
+
+# print("Day 1 -Python print Function")
+
+# print("The function in declared like this:")
+
+# print("print('what to print')")
+
+# String Manipulation
+
+# print("Dawit " + "Mellese")
+
+# Debbuging
+
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the " + " sign.")
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+# Input Function and Nested Function
+
+# print(len(input("what is you name?")))
+
+
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+temp = a
+a = b
+b = temp
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
+
+
+
+
+
+
+
---
+++ day-01/project1.py
@@ -0,0 +1,12 @@
+
+# Print the Band name generator
+print("Welcome to the Band Name Generator.")
+
+#  Input the city name
+city_name = input("What's name of the city you grew up in?\n")
+
+#  Input the pet Name
+pet_name = input("what's ypur pet's name?\n")
+
+#  Finally Concatenate
+print ("Your band name could be " + city_name + " " + pet_name )
