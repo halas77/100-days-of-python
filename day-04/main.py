ca83744ccb3010d7ca2fceab7cf878c4d83fd3f1
Dawit Mellese <dawitm777@gmail.com>
2023-12-20 21:23:24 +0300

    day 04 commit

---
+++ day-04/main.py
@@ -0,0 +1,50 @@
+import random
+
+# random_float = random.random()
+# print(random_float)
+
+# number = random.randint(1, 2)
+
+
+# if number == 1:
+#     print("Head")
+# else:
+#     print("Tail.")
+
+
+# Lists
+
+ 
+# names_string = input("Give me everybody's names separated by a comma.")
+# names = names_string.split(", ")
+
+# selected_number = random.randint(0, len(names)-1)
+
+# selected_person = names[selected_number]
+
+# print(f"{selected_person} is seleced.")
+
+
+# row1 = [" ", " ", " "]
+# row2 = [" ", " ", " "]
+# row3 = [" ", " ", " "]
+
+# map = [row1, row2, row3]
+
+# position = input(f"Where do you want to insert the treasure? ")
+
+# vertical  = int(position[1])
+# horizontal = int(position[0])
+
+# selected_row = map[vertical - 1]
+# selected_row[horizontal - 1] = "X"
+
+
+# print(f"{row1}\n{row2}\n {row3}")
+
+
+
+
+
+
+
---
+++ day-04/project4.py
@@ -0,0 +1,46 @@
+import random
+
+rock = "Rock"
+paper = "Paper"
+scissors = "Scissors"
+
+
+combined = [rock, paper, scissors]
+
+user_input = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors \n")
+
+user_input_as_int = int(user_input)
+
+random_number = random.randint(0, 2)
+
+computer = combined[random_number]
+
+if user_input_as_int == random_number:
+    print("Same Choices. No win No lose.")  
+    
+      
+if user_input_as_int == 0 and random_number == 1:
+    print("You lose.")
+    
+    
+if user_input_as_int == 1 and random_number == 2:
+    print("You lose.")
+    
+    
+if user_input_as_int == 1 and random_number == 0:
+    print("You Won.")
+    
+    
+if user_input_as_int == 2 and random_number == 1:
+    print("You Won.")
+    
+    
+if user_input_as_int == 0 and random_number == 2:
+    print("You won.") 
+    
+    
+if user_input_as_int == 2 and random_number == 0:
+    print("You lose.")
+
+
+print(f"{random_number} and {user_input}")
