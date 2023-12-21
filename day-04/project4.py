59de1c3e663eb23ee3fa84275a2752b6ba94c387
Dawit Mellese <dawitm777@gmail.com>
2023-12-21 19:00:33 +0300

    some changes

--- day-04/project4.py
+++ day-04/project4.py
@@ -15,32 +15,29 @@ random_number = random.randint(0, 2)
 
 computer = combined[random_number]
 
+
+
+
+
 if user_input_as_int == random_number:
     print("Same Choices. No win No lose.")  
-    
-      
+         
 if user_input_as_int == 0 and random_number == 1:
     print("You lose.")
-    
-    
+       
 if user_input_as_int == 1 and random_number == 2:
     print("You lose.")
-    
-    
+       
 if user_input_as_int == 1 and random_number == 0:
     print("You Won.")
-    
-    
+       
 if user_input_as_int == 2 and random_number == 1:
     print("You Won.")
-    
-    
+      
 if user_input_as_int == 0 and random_number == 2:
     print("You won.") 
     
-    
 if user_input_as_int == 2 and random_number == 0:
     print("You lose.")
-
 
 print(f"{random_number} and {user_input}")
