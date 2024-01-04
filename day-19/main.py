4748fafad14fddd9dcc2de147149c9b719886a30
Dawit Mellese <dawitm777@gmail.com>
2024-01-04 22:16:46 +0300

    day 18 commit

---
+++ day-19/main.py
@@ -0,0 +1,40 @@
+# from turtle import Turtle, Screen
+
+# tim = Turtle()
+# screen = Screen()
+
+# def move_forward():
+#     tim.setheading(0)
+#     tim.forward(10)
+
+# def move_backward():
+#     tim.setheading(180)
+#     tim.forward(10)
+    
+# def move_upward():
+#     tim.setheading(90)
+#     tim.forward(10)
+    
+# def move_downward():
+#     tim.setheading(-90)
+#     tim.forward(10)
+    
+# def clear():
+#     tim.clear()
+
+ 
+# screen.listen()
+
+# def draw_object():    
+#     screen.onkeypress(key="w", fun=move_upward) 
+#     screen.onkeypress(key="s", fun=move_downward) 
+#     screen.onkeypress(key="a", fun=move_backward) 
+#     screen.onkeypress(key="d", fun=move_forward)
+#     screen.onkeypress(key="c", fun=clear)   
+
+# draw_object()    
+# screen.exitonclick()
+
+
+
+
---
+++ day-19/project.py
@@ -0,0 +1,45 @@
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which will win the race? Enter a color: ")
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+turtle_lists = []
+turtle_name = ["tim", "lam", "kam", 'jam', 'dam', 'fam']
+
+for turtle in turtle_name:
+    turtle = Turtle(shape="turtle")
+    turtle_lists.append(turtle)
+
+i = 0
+y = -100
+
+print(turtle_lists)
+
+for list in turtle_lists: 
+    list.color(colors[i])
+    list.penup()
+    list.goto(x=-230, y= y)
+    y += 30
+    i += 1
+    
+is_on = False
+
+if user_bet:
+    is_on = True
+    
+while is_on:
+    for list in turtle_lists:
+        if list.xcor() > 230:
+            is_on = False
+            winner = list.pencolor()
+            if winner == user_bet:
+                print(f"You are the winner.")
+            else:
+                print(f"You lose. The {winner} color is winner.")
+        dis = random.randint(0, 10)
+        list.forward(dis)
+
+
+screen.exitonclick()
