f4da5ac31bc88cec8fefed8e82e0a350bcf452df
Dawit Mellese <dawitm777@gmail.com>
2024-01-13 22:53:31 +0300

    day-28 commit

---
+++ day-28/main.py
@@ -0,0 +1,78 @@
+from tkinter import *
+import math
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 1
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ----------------------------- # 
+
+def timer_starter():
+    global reps
+    reps += 1
+    
+    work_sec = WORK_MIN * 60
+    short_break = SHORT_BREAK_MIN * 60
+    long_break = LONG_BREAK_MIN * 60
+    
+    if reps % 8 == 0:
+        count_down(long_break)
+        timer.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        timer.config(text="Break", fg=PINK)
+        count_down(short_break)
+    else:
+        timer.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+        
+    
+# -------------------------- COUNTDOWN MECHANISM ------------------------- # 
+
+def count_down(count):
+    min = math.floor(count / 60)
+    sec = count % 60
+    if sec < 10:
+        sec = f"0{sec}"
+        
+    
+    canvas.itemconfig(timer_text, text=f"{min}:{sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        timer_starter()
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro") 
+window.config(padx=100, pady=50, bg=YELLOW)
+
+timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+timer.grid(column=1, row=0)
+
+checked = Label(text="done", fg=GREEN, bg=YELLOW)
+checked.grid(column=1, row=3)
+
+start = Button(text="Start",  fg="#000000", command=timer_starter)
+start.grid(column=0, row=2)
+
+
+Reset = Button(text="Reset",  fg="#000000")
+Reset.grid(column=2, row=2)
+
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="./day-28/tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text  =canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+window.mainloop()
