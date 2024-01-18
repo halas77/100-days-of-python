2b3fb1c6e7a8b833caa5bf7f6a2e4157c0ecfe2c
Dawit Mellese <dawitm777@gmail.com>
2024-01-18 22:22:31 +0300

    day 32 commit

--- day-32/main.py
+++ day-32/main.py
@@ -1,14 +1,35 @@
 import smtplib
+import datetime as dt
+import random
 
 my_email = "dawitmellese23@gmail.com"
 password = "1234abcd()"
 
-
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
-connection.login(user=my_email, password=password)
+connection.login(my_email, password)
 
-connection.sendmail(
-    from_addr=my_email,
-    to_addrs="dawitm777@gmail.com", msg="Subject:Hello\n\nThis is the body section"
-)
+data_of_birth = dt.datetime(year=2000, month=1, day=12)
+
+print(data_of_birth)
+
+
+with open("./day-32/quotes.txt") as file:
+    all_data = file.readlines()
+    data = random.choice(all_data)
+    
+
+now = dt.datetime.now()
+
+if now.weekday() == 0:
+
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=my_email, msg=f"Subject:Boom!\n\n {data}"
+    )
+    connection.close()
+    
+else:
+    print("Not monday")
+
+
