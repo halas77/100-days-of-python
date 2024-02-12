1e02765eea9a84369e2299a67090fe5c3a1e3c55
Dawit Mellese <dawitm777@gmail.com>
2024-02-12 21:20:00 +0300

    day 60 commit

--- day-60/main.py
+++ day-60/main.py
@@ -2,16 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    name = request.form['username']
-    password = request.form['password']
-    return f'Name:{name} Password:{password}'
+    if request.method == 'POST':
+        name = request.form['username']
+        password = request.form['password']
+        return f'Name:{name} Password:{password}'
+    
+    return render_template('index.html')
     
     
 if __name__ == '__main__':
