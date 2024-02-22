a859e9f56161b998c01873aa895ac83ec7feb7d3
Dawit Mellese <dawitm777@gmail.com>
2024-02-22 21:02:48 +0300

    day 71 final commit

--- day-71/main.py
+++ day-71/main.py
@@ -16,3 +16,22 @@ cleared_df = df.dropna()
 # print(cleared_df['Undergraduate Major'].loc[cleared_df['Starting Median Salary'].idxmin()])
 
 
+
+# spread_col = cleared_df['Mid-Career 90th Percentile Salary'] - cleared_df['Mid-Career 10th Percentile Salary']
+# cleared_df.insert(1, 'Spread', spread_col)
+
+# low_risk = cleared_df.sort_values('Spread')
+
+
+# print(low_risk['Undergraduate Major'][0])
+# print(low_rist[['Undergraduate Major', 'Spread']].head())
+
+
+# maxi = cleared_df['Spread'].idxmax()
+
+# maxi = cleared_df['Undergraduate Major'].loc()
+
+
+# print(maxi)
+
+# print(cleared_df.groupby('Group').count())
